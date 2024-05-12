58b5904979ab95c5cead2e187cc7319e9ba00456
RADU ANDREI NEGULESCU <radu.negulescu@s.unibuc.ro>
2024-05-12 15:24:37 +0300

    Add project files.

---
+++ WineViewWeb2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WineViewWeb2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
